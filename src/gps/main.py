7d8f95038df82dc40ad3cc078be6af481773d9d7
Luis <luisgonzalezsalazar03@gmail.com>
2024-11-06 20:19:44 -0400

    Roboto definitivo

--- src/gps/main.py
+++ src/gps/main.py
@@ -1,25 +1,36 @@
+import io
+
 import serial 
 import time
 import string
 import pynmea2
 
+last_known_location={
+	'lat': 0,
+	'lng': 0
+}
+
 def get_gps_location():
 	port="/dev/ttyAMA0"
 	ser=serial.Serial(port, baudrate=9600, timeout=0.5)
-	dataout = pynmea2.NMEAStreamReader()
-	newdata = str(ser.readline(), errors="replace")
+	sio= io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 
-	print(newdata)
-	if "$GNGGA" in newdata:
-		newmsg=pynmea2.parse(newdata)
-		dic = {
-			'lat': newmsg.latitude,
-			'lng': newmsg.longitude
+	try:
+		line=sio.readline()
+		msg=pynmea2.parse(line)
+		dic={
+			'lat': msg.latitude,
+			'lng': msg.longitude
 		}
 		print(dic)
+		global last_known_location
+		last_known_location=dic
 		return dic
-		
-	return {
-		'lat': 0,
-		'lng': 0
-	}
+	except serial.SerialException as e:
+		print("Device error: {}".format(e))
+	except pynmea2.ParseError as e:
+		print("Parse error: {}".format(e))
+	except Exception as e:
+		print("Unknown error: {}".format(e))
+
+	return last_known_location
